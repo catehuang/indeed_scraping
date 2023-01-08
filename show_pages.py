1b534c03241005de5a3fa44e341d93a7f397540e
catehuang <s0350228@gmail.com>
2023-01-07 17:24:13 -0700

    changed to a class with its methods

--- show_pages.py
+++ show_pages.py
@@ -1,21 +1,23 @@
 import pandas as pd
 import os
-from flask import Flask, render_template, url_for, request, redirect, abort, flash
+import sys
+from flask import Flask, render_template, url_for, redirect
 from flask_bootstrap import Bootstrap
-from dotenv import load_dotenv
 
-# setup flask
 app = Flask("__name__")
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 Bootstrap(app)
 PORT = 5000
 
-# deal with data, using dictionary is better than the class since there are lots of efforts needed to do
-# between html and js
+file_name = sys.argv[1]
+
+if file_name is None:
+    sys.exit("No json file!\n")
+else:
+    print(f"using {file_name}")
+
 jobs = []
-file_path = "logs/job-2023-01-05-180256.csv"
-df = pd.read_csv(file_path, encoding='utf8')
-# technically the max index of the row is df.shape[0], but the first row in header which is not what we want
+df = pd.read_csv(file_name, encoding='utf8')
 for row_index in range(0, df.shape[0]):
     job = {
         'title': df.iloc[row_index][0],
@@ -26,8 +28,10 @@ for row_index in range(0, df.shape[0]):
     }
     jobs.append(job)
 
+print(f"{jobs}")
+
 total_number_of_jobs = df.shape[0]
-total_pages = int(len(jobs) / 15)
+total_pages = len(jobs) / 15
 if len(jobs) % 15 > 0:
     total_pages += 1
 
@@ -41,7 +45,7 @@ def home():
 def show_page(page_id):
     if 0 < page_id <= total_pages:
         current_job_list = jobs[(page_id - 1) * 15: page_id * 15 - 1]
-        return render_template("page.html", total_number_of_jobs=total_number_of_jobs, page_id=page_id,
+        return render_template("./page.html", total_number_of_jobs=total_number_of_jobs, page_id=page_id,
                                jobs=current_job_list, total_pages=total_pages)
     else:
         return redirect(url_for('show_page', page_id=1))
@@ -53,5 +57,5 @@ def handle_exception(e):
     return redirect('/')
 
 
-if __name__ == "__show_pages__":
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=PORT)
