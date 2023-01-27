49a93b73a706b3c3576a1c36af043ac29d894bfe
catehuang <s0350228@gmail.com>
2023-01-27 11:46:18 -0700

    dealt with the case if the job expired on Indeed; added the option to filter out the requirement on years expereince

--- scrap_data.py
+++ scrap_data.py
@@ -93,12 +93,13 @@ class DataCollection:
             job_count_span = job_count_block.find_elements(By.CSS_SELECTOR, 'span')
             job_count = job_count_span.__getitem__(0).text
             job_number = re.sub('[^0-9]', '', job_count)
-            real_job_number = int(int(job_number)/1.5)
+            real_job_number = int(int(job_number) / 1.5)
             print(f"Total number of jobs on Indeed is {job_number}, "
                   f"and there are about {math.ceil(int(job_number) / 15)} pages based on the total number.\n"
-                  f"(However, the real number of jobs could be between {real_job_number} and, {job_number}"
-                  f", and total pages is between {math.ceil(real_job_number/15)} and {math.ceil(int(job_number)/15)})"
-                  f". One page needs about 1 - 3 minutes to walk through.\n")
+                  f"Generally, one page needs about 1 - 3 minutes to walk through.\n"
+                  f"(However, the real number of jobs would be between {real_job_number} and {job_number}"
+                  f", and total pages is between {math.ceil(real_job_number/15)} and {math.ceil(int(job_number)/15)})\n"
+                  )
             return real_job_number
         except NoSuchElementException:
             # raise ValueError("Can't find any result for the job title!")
@@ -189,11 +190,11 @@ class DataCollection:
                 counter += 1
 
             if re.search(rf"({rule_string})", title, re.I):
-                print(f"included exclude keywords - {title}")
+                print(f"included excluded keywords - {title}")
                 return False
         return True
 
-    def filter_out_by_description(self, job_description):
+    def filter_out_by_description(self, job_title, job_description):
         if len(os.getenv('FILTER_OUT_JOB_DESC').split(" ")) != 0:
             excluding_certain_words = os.getenv('FILTER_OUT_JOB_DESC').split(" ")
             rule_string = ""
@@ -205,7 +206,34 @@ class DataCollection:
                 counter += 1
 
             if re.search(rf"({rule_string})", job_description, re.I):
+                print(f"filtered out due to job description contains keywords - {job_title}")
                 return True
+
+            # if description required professional experience, get the line
+            if len(os.getenv('FILTER_OUT_BY_MIN_REQ_YEARS')) > 0:
+                years = int(os.getenv('FILTER_OUT_BY_MIN_REQ_YEARS').split(" ")[0])
+                line_pattern1 = re.search(rf"(\d.*years.* ?(of)? experience)", job_description, re.I)
+                line_pattern2 = re.search(rf"experience.*: \d.*years", job_description, re.I)
+
+                if line_pattern1 is not None or line_pattern2 is not None:
+                    if line_pattern1 is not None:
+                        line = line_pattern1
+                    else:
+                        line = line_pattern2
+
+                    # get the number, it could be a number or a range of year(in this case, get the first number)
+                    min_required_years = re.findall(r"\d", line[0])[0]
+                    # print(f"(## Debug {line[0]}, {min_required_years})")
+
+                    # ignore this job, if it needs more than n years' of professional experience
+                    # make sure this line isn't company's history
+                    try:
+                        if 100 > int(min_required_years) > years:
+                            print(f"filtered out due to '{line[0]}' - {job_title}")
+                            return True
+                    except ValueError as e:
+                        print(e.__traceback__)
+                        pass
         return False
 
     def scraping_a_page(self):
@@ -224,46 +252,58 @@ class DataCollection:
                 job_result_block = job_on_list.find_element(By.CSS_SELECTOR, 'td') \
                                    and job_on_list.find_element(By.CLASS_NAME, 'resultContent')
                 job_title_block = job_result_block.find_element(By.CSS_SELECTOR, 'h2')
+
                 # filter jobs by their titles
                 if self.is_qualified(job_title_block.text):
                     company_name_block = job_result_block.find_element(By.CLASS_NAME, 'companyName')
                     company_location_block = job_result_block.find_element(By.CLASS_NAME, 'companyLocation')
                     job_title_block.click()
                     time.sleep(2)
+
                     # right column - content
                     right_panel = main_block.find_element(By.CLASS_NAME, 'jobsearch-RightPane')
 
                     # grep the apply link
                     apply_button_block = right_panel.find_element(By.ID, 'jobsearch-ViewJobButtons-container')
-                    # there are two kinds of button/link for applying a job
 
+                    # there are two kinds of button/link for applying a job
+                    job_expired = False
                     try:
                         # 1) is an anchor link for applying on company site
                         apply_button_area = apply_button_block.find_element(By.ID, 'applyButtonLinkContainer')
-                        link = apply_button_area.find_elements(By.CSS_SELECTOR, 'a').__getitem__(0).get_attribute('href')
+                        link = apply_button_area.find_elements(By.CSS_SELECTOR, 'a').__getitem__(0).get_attribute(
+                            'href')
                     except NoSuchElementException:
-                        # 2) is a button - Apply now; copy the page url
-                        apply_button_area = apply_button_block.find_element(By.CLASS_NAME, 'ia-IndeedApplyButton')
-                        link = apply_button_area.find_element(By.CSS_SELECTOR, 'span') \
-                            .get_attribute('data-indeed-apply-joburl')
+                        try:
+                            # 2) is a button - Apply now; copy the page url
+                            apply_button_area = apply_button_block.find_element(By.CLASS_NAME, 'ia-IndeedApplyButton')
+                            link = apply_button_area.find_element(By.CSS_SELECTOR, 'span') \
+                                .get_attribute('data-indeed-apply-joburl')
+                        except NoSuchElementException:
+                            # 3) the job expired! there's no any button
+                            print(f"skip due to the job expired {job_title_block.text}")
+                            job_expired = True
 
-                    job_content_block = main_block.find_element(By.CLASS_NAME, 'jobsearch-JobComponent-description')
-                    job_description = job_content_block.find_element(By.ID, 'jobDescriptionText')
+                    if not job_expired:
+                        job_content_block = main_block.find_element(By.CLASS_NAME, 'jobsearch-JobComponent-description')
+                        job_description = job_content_block.find_element(By.ID, 'jobDescriptionText')
 
-                    # add another filter for the content of the job description
-                    if not self.filter_out_by_description(job_description.text):
-                        print(f"Found the job as {job_title_block.text} - {link}")
-                        # which data type is better to manipulate data?
-                        # use arrays to save different columns, then use pandas to combine them as csv or json
-                        # use objects to store jobs, then covert them to json (good to store to db)
-                        # use json for each job (generate correct format is not easy)
-                        job = Job(job_title_block.text, company_name_block.text, link, company_location_block.text,
-                                  job_description.text)
-                        jobs.append(job)
+                        # add another filter for the content of the job description
+                        if not self.filter_out_by_description(job_title_block.text, job_description.text):
+                            print(f"Found the job as {job_title_block.text} - {link}")
+
+                            # which data type is better to manipulate data?
+                            # use arrays to save different columns, then use pandas to combine them as csv or json
+                            # use objects to store jobs, then covert them to json (good to store to db)
+                            # use json for each job (generate correct format is not easy)
+                            job = Job(job_title_block.text, company_name_block.text, link, company_location_block.text,
+                                      job_description.text)
+                            jobs.append(job)
+
                 # scroll down for each job element
                 self.driver.execute_script("arguments[0].scrollIntoView();", job_on_list)
         except NoSuchElementException as e:
-            print("something wrong about recognizing elements\n")
+            print("something wrong about recognizing elements!!!\n")
             print(e.msg)
             pass
         return jobs
